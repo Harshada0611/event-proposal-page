d990bd7f1280ce5a4473dd3006431abc622a37a4
Harshada0611 <harshadaa1997@gmail>
2023-03-01 13:16:40 +0530

    Added registration signin and middleware route for vendor

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+
 const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000;
 //configuring .env
 dotenv.config();
 //connect to DB
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('connected to DB')
-})
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{console.log('Connected to DB')})
+.catch((err)=>{console.log('Mongo connection failed',err)})
 
 
 app.listen(port, () => console.log('Server is running on port:', port));
