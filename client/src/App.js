f0e3384a2abf3fdb5af9f0dd9df719678c29a333
Ashwin Jose <ajashwin8880@gmail.com>
2023-03-01 20:20:50 +0530

    added proposal route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import CreateProposal from './Components/createProposal/CreateProposal';
+import ProposalsList from './Components/proposalList/ProposalList';
+
 
 
 function App() {
   return (
     <div className="App">
+    {/* <CreateProposal/> */}
+    <ProposalsList/>
     </div>
   );
 }
--- server/routes/proposal.js
+++ server/routes/proposal.js
@@ -22,5 +22,4 @@ router.post("/", async(req,res) => {
 });
 
 
-
 module.exports = router;
