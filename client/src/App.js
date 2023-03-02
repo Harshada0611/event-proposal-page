a9ab0c4b1d51e5159d981f1b28e713c0060cf7c4
Ashwin Jose <ajashwin8880@gmail.com>
2023-03-02 16:03:14 +0530

    Added create and list

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react'
-
+// import AllProposalList from './components/All-proposal-List/AllProposalList';
+import CreateProposal from './components/createProposal/CreateProposal';
 
 function App() {
   return (
     <div className='App'>
+      <CreateProposal/>
     </div>
   );
 }
